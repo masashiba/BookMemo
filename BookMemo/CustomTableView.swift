6903308d2be001ab9a203aeee5d4a9dc87cebb77
Shiba <masashibaprograming@gmail.com>
2019-08-29 22:07:57 +0900

    デザイン調整

---
+++ BookMemo/CustomTableView.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableView.swift
+//  BookMemo
+//
+//  Created by 芝本　将豊 on 2019/08/26.
+//  Copyright © 2019 芝本　将豊. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableView: UITableView {
+
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.next?.touchesBegan(touches, with: event)
+//    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
